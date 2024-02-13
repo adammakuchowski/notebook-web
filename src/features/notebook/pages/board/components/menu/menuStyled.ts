f0003cd9906bfe8dc8b8fe8406625a16c36743a5
Adam Makuchowski <maku971@gmail.com>
2024-02-13 20:30:44 +0100

    init fsd architecture

--- src/features/notebook/pages/board/components/menu/menuStyled.ts
+++
@@ -1,8 +0,0 @@
-import styled from '@emotion/styled'
-
-export const MenuContainer = styled.div`
-  width: 10%;
-  background-color: blue;
-  display: flex;
-  justify-content: center;
-`
--- src/features/notebook/pages/board/components/notes/Notes.tsx
+++
@@ -1,5 +0,0 @@
-export const Notes = () => {
-  return (
-    <div>Notes</div>
-  )
-}
