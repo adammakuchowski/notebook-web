850afe81466954e48b5940f0c19f74453fd04391
Adam Makuchowski <maku971@gmail.com>
2024-03-23 17:52:54 +0100

    add TODOs

--- src/hocs/withAuth/with-auth.tsx
+++ src/hocs/withAuth/with-auth.tsx
@@ -3,6 +3,7 @@ import {Loader} from '@mantine/core'
 import {useAuth} from 'hooks'
 
 export const withAuth = (WrappedComponent: () => JSX.Element) => (props: Record<string, unknown>) => {
+  // TODO: implement refreshing the toekn
   const {isTokenValid} = useAuth()
 
   if (!isTokenValid) {
