24592cd26607543cbf479c74e701040542db16db
Adam Makuchowski <maku971@gmail.com>
2024-03-30 11:22:10 +0100

    init login redirection

--- src/hocs/withAuth/with-auth.tsx
+++ src/hocs/withAuth/with-auth.tsx
@@ -3,11 +3,7 @@ import {Loader} from '@mantine/core'
 import {useAuth} from 'hooks'
 
 export const withAuth = (Component: () => JSX.Element) => (props: Record<string, unknown>) => {
-  // TODO: implement refreshing the toekn
-  // TODO: useAuth - should be useTokenValid
   const {isTokenValid} = useAuth()
-
-  console.log('isTokenValid', isTokenValid)
 
   if (!isTokenValid) {
     return (
